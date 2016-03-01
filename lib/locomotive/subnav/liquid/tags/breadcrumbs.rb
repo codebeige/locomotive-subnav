fcc6dab07671a4d06333fa7e3deebb32d717c2ed
Tibor Claassen <tibor@gmah.net>
2016-03-01 17:33:42 +0100

    Render nav item for current page

--- lib/locomotive/subnav/liquid/tags/breadcrumbs.rb
+++ lib/locomotive/subnav/liquid/tags/breadcrumbs.rb
@@ -3,14 +3,41 @@ module Locomotive
     module Liquid
       module Tags
         class Breadcrumbs < ::Liquid::Tag
+
           def render(context)
-            <<-HTML
-            <nav class="breadcrumbs">
-              <ul>
-                <li class="nav-item"><a href="/foo/bar">FOO | BAR</a></li>
-              </ul>
-            </nav>
-            HTML
+            current_page = context.registers[:page]
+            wrapper do
+              level('current') do
+                attrs = page_attributes(current_page, context)
+                item 'current', attrs[:title], attrs[:path]
+              end
+            end
+          end
+
+          protected
+
+          def indent(markup)
+            %(\n#{markup.gsub(/^/, '  ')}\n)
+          end
+
+          def wrapper
+            %(<nav class="breadcrumbs">#{indent yield}</nav>)
+          end
+
+          def level(name = nil)
+            attrs = if name then %( class="#{name}") else '' end
+            %(<ul#{attrs}>#{indent yield}</ul>)
+          end
+
+          def item(name = nil, title, path)
+            names = ['nav-item']
+            names << name if name
+            %(<li class="#{names.join ' '}"><a href="#{path}">#{title}</a></li>)
+          end
+
+          def page_attributes(page, context)
+            url_builder = context.registers[:services].url_builder
+            {title: page.title, path: url_builder.url_for(page)}
           end
 
         ::Liquid::Template.register_tag('breadcrumbs'.freeze, Breadcrumbs)
--- spec/locomotive/subnav/liquid/tags/breadcrumbs_spec.rb
+++ spec/locomotive/subnav/liquid/tags/breadcrumbs_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
 
 describe Locomotive::Subnav::Liquid::Tags::Breadcrumbs do
-  let(:context) { ::Liquid::Context.new }
+  let(:url_builder) do
+    instance_double Locomotive::Steam::UrlBuilderService, url_for: '/foo/bar'
+  end
+  let(:services) { double 'Services', url_builder: url_builder }
+  let(:site) { instance_double Locomotive::Steam::Site }
+  let(:page) { double 'Page', title: 'Example Page' }
+  let(:registers) { {site: site, page: page, services: services} }
+  let(:context) { ::Liquid::Context.new({}, {}, registers) }
 
   def render(source, options = {})
     Locomotive::Steam::Liquid::Template.parse(source, options).render(context)
@@ -13,6 +20,22 @@ describe Locomotive::Subnav::Liquid::Tags::Breadcrumbs do
     it 'renders container' do
       rendered = render(source)
       expect(rendered).to have_tag('nav.breadcrumbs')
+    end
+
+    it 'renders current level' do
+      rendered = render(source)
+      expect(rendered).to have_tag('nav ul.current')
+    end
+
+    it 'renders item for current page' do
+      allow(url_builder).to receive(:url_for).with(page) { '/prunus/avium'  }
+      allow(page).to receive(:title) { 'Wild Cherry' }
+      rendered = render(source)
+      expect(rendered).to have_tag('ul.current li.nav-item.current') do
+        with_tag 'a',
+          with: {href: '/prunus/avium'},
+          text: 'Wild Cherry'
+      end
     end
   end
 end
