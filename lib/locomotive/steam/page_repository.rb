47756d77f4c4d4b41bb8a59ae5d274875aba5960
Tibor Claassen <tibor@gmah.net>
2016-03-09 18:02:15 +0100

    Fetch ancestors with children

--- lib/locomotive/steam/page_repository.rb
+++ lib/locomotive/steam/page_repository.rb
@@ -2,7 +2,7 @@ module Locomotive
   module Steam
     class PageRepository
       def ancestors_with_children(page, level = 0)
-        ancestors_of(page)
+        all(k(:parent_id, :in) => page.parent_ids + [page._id, nil])
       end
     end
   end
--- spec/locomotive/steam/page_repository_spec.rb
+++ spec/locomotive/steam/page_repository_spec.rb
@@ -3,24 +3,61 @@ require 'locomotive/steam/adapters/filesystem'
 
 describe Locomotive::Steam::PageRepository, type: :repository do
   describe '#ancestors_with_children' do
-    let(:collection) { [page_attributes(_id: '2.2', title: 'Wild Cherry')] }
-    let(:page) { double 'Page', _id: '2.2', parent_ids: [] }
+    let(:collection) do
+      [
+        page_attributes(_id: '0',     _fullpath: ''),
+        page_attributes(_id: '1',     _fullpath: 'apples'),
+        page_attributes(_id: '1.1',   _fullpath: 'apples/gala'),
+        page_attributes(_id: '1.2',   _fullpath: 'apples/jonagold'),
+        page_attributes(_id: '2',     _fullpath: 'pears'),
+        page_attributes(_id: '2.1',   _fullpath: 'pears/snow-pear'),
+        page_attributes(_id: '3',     _fullpath: 'cherries'),
+        page_attributes(_id: '3.1',   _fullpath: 'cherries/hill-cherry'),
+        page_attributes(_id: '3.1.1', _fullpath: 'cherries/hill-cherry/fruit'),
+        page_attributes(_id: '3.2',   _fullpath: 'cherries/wild-cherry'),
+        page_attributes(_id: '3.2.1', _fullpath: 'cherries/wild-cherry/fruit'),
+        page_attributes(_id: '3.3',   _fullpath: 'cherries/taiwan-cherry'),
+      ]
+    end
 
-    def ids(set)
-      set.map(&:_id)
+    let(:page) do
+      double 'Page', _id: '3.2',
+                     fullpath: 'cherries/wild-cherry',
+                     parent_ids: ['0', '3']
+    end
+
+    def ids(records)
+      records.map(&:_id)
     end
 
     it 'includes current page' do
       result = repository.ancestors_with_children(page)
-      expect(ids result).to include('2.2')
+      expect(ids result).to include('3.2')
     end
 
     it 'incudes ancestor pages' do
-      collection << page_attributes(_id: '0')
-      collection << page_attributes(_id: '1.1')
-      allow(page).to receive(:parent_ids) { ['0', '1.1'] }
       result = repository.ancestors_with_children(page)
-      expect(ids result).to include('0', '1.1')
+      expect(ids result).to include('3', '0')
+    end
+
+    it 'includes siblings of ancestors' do
+      result = repository.ancestors_with_children(page)
+      expect(ids result).to include('1', '2')
+    end
+
+    it 'includes children of ancestors' do
+      result = repository.ancestors_with_children(page)
+      expect(ids result).to include('3.1', '3.3')
+    end
+
+    it 'includes children of self' do
+      result = repository.ancestors_with_children(page)
+      expect(ids result).to include('3.2.1')
+    end
+
+    it 'does not include children of siblings' do
+      result = repository.ancestors_with_children(page)
+      expect(ids result).not_to include('1.1', '1.2', '2.1', '3.1.1')
     end
   end
 end
